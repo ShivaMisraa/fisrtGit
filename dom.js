b6e4c3b74c469b535a482b86da1b31c299f8992f
ShivaMisraa <shivam18557@gmail.com>
2023-01-23 00:05:33 +0530

    this is for GetElement by class Name

---
+++ dom.js
@@ -0,0 +1,21 @@
+// console.dir(document);
+// // EXAMINE THE DOCUMENT OBJECT //
+// // console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// //document.title 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all [10]);
+// document.all[10].textContent = 'Hello';
+var headerTitle = document.getElementById('header-title');
+var header= document.getElementById('main-header');
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = 'Goodbye';
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML = '<h3>Hello</h3>';
+header.style.borderBottom = 'solid 3px #000';
